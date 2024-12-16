f1dd9ebfb9e3f14b702eb1d7012e50fa2d3e4f52
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-12-16 17:16:00 -0600

    we scrollin baby

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 
 export default function Index() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+    },
+  });
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.title}>Murphy</Text>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </ScrollView>
     </View>
   );
 }
